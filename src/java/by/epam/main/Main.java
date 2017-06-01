cdfd4118d0497fee7947ea220f7a99fea123bb56
Yayheniy_Lepkovich <Yayheniy_Lepkovich@epam.com>
2017-06-01 13:30:49 +0300

    implement main class to print 'Hello world'

---
+++ src/java/by/epam/main/Main.java
@@ -0,0 +1,10 @@
+package by.epam.main;
+
+/**
+ * Created by Yayheniy_Lepkovich on 6/1/2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
